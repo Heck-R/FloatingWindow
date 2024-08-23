fa19af55f32b4a799355f187f191a5e8545b3c7a
Heck-R <hecvid97@gmail.com>
2024-08-23 02:01:39 +0200

    CHORE: Debug test adjusted

--- test.js
+++ test.js
@@ -1,28 +1,30 @@
-/**/
-let floatingWindow = document.createElement("floating-window");
-console.log(floatingWindow);
-document.body.appendChild(floatingWindow);
+function run() {
+	/**/
+	/** @type FloatingWindow */
+	let floatingWindow = document.createElement("floating-window");
+	document.body.appendChild(floatingWindow);
+	console.log(floatingWindow);
 
-floatingWindow.initContent = content => {
-	// console.log("asd");
-	// content.innerHTML = `
 	floatingWindow.content.innerHTML = `
-    asd<b>asds</b>asd<i>adaaa</i>aaaaaa<hr>
-    <table>
-    <tr><th>asd</th><th>asd</th><th>asd</th></tr>
-    <tr><td>asd</td><td>asd</td><td>asd</td></tr>
-    <tr><td>asd</td><td>asd</td><td>asd</td></tr>
-    </table>
-    <textarea>
-    </textarea>
-    aaaaaaaaaaa
-    <a href="https://google.com">link</a>
-    <button>asd</button>aaaaaaaaaasd<br>
-    klsjg`;
+        asd<b>asds</b>asd<i>adaaa</i>aaaaaa<hr>
+        <table>
+        <tr><th>asd</th><th>asd</th><th>asd</th></tr>
+        <tr><td>asd</td><td>asd</td><td>asd</td></tr>
+        <tr><td>asd</td><td>asd</td><td>asd</td></tr>
+        </table>
+        <textarea>
+        </textarea>
+        aaaaaaaaaaa
+        <a href="https://google.com">link</a>
+        <button>asd</button>aaaaaaaaaasd<br>
+        klsjg`;
 
 	floatingWindow.style.height = "300px";
-};
-//floatingWindow.content.setAttribute('contenteditable', 'true')
+	// };
+	//floatingWindow.content.setAttribute('contenteditable', 'true')
 
-// document.body.appendChild(floatingWindow);
-/**/
+	// document.body.appendChild(floatingWindow);
+	/**/
+}
+
+run();
